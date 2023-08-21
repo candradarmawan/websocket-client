832ee335d497c3566cc5caa80a09ac09a0ae3bcf
chandra darmawan <chandradarmawan17@gmail.com>
2023-08-22 01:31:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Comments from "./components/Comments";
+import Header from "./components/Header";
+
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:4000");
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [selectedUserId, setSelectedUserId] = useState("");
+
+  async function fetchUsers() {
+    const result = await fetch("http://localhost:4000/users");
+    const users = await result.json();
+
+    setUsers(users);
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        users={users}
+        selectedUserId={selectedUserId}
+        setSelectedUserId={setSelectedUserId}
+      />
+      <Comments socket={socket} selectedUserId={selectedUserId} />
     </div>
   );
 }
